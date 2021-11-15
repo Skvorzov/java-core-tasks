351b1a1601b382af8b7f7da3f9561a0e5546076d
Skvortsov <skvorzov.sv@gmail.com>
2021-11-15 13:20:32 +0200

    homework 15.11.2021

--- src/main/java/com/java/training/Factorial.java
+++ src/main/java/com/java/training/Factorial.java
@@ -6,13 +6,9 @@ public class Factorial {
      * Реализуйте метод getFactorial(int number) который вовзращает факториал числа.
      */
     public int getFactorial(int number) {
-        int[] arr = new int[number];
-        int num = 1;
         int sum = 1;
-        for (int i = 0; i < arr.length ; i++) {
-            arr[i] = num;
-            num++;
-            sum = sum * arr[i];
+        for (int i = 0; i < number; i++) {
+            sum = sum * (i + 1);
         }
         return sum;
     }
